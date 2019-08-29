5254fa4c3ac120f71e69c62778f79cb3c421114a
rachelbryant <rachel.bryant@wustl.edu>
2019-08-29 12:08:09 -0500

    Average

--- src/studio1/Hello.java
+++ src/studio1/Hello.java
@@ -6,11 +6,8 @@ public class Hello {
 	
 	public static void main(String[] args) {
 		ArgsProcessor ap = new ArgsProcessor(args);
-		
-		String mood = ap.nextString("How are you feeling today?");
-		
-		System.out.println("Hello and welcome to CSE 131!");
-		System.out.println("You seem to be feeling " + mood);
+
+		int n1 = ap.nextInt();
 	}
 
 }
